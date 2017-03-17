ca456cca7877d494837a465e0bf765a1aa4c08f5
Jeremy Singer-Vine <jsvine@gmail.com>
2017-03-17 18:35:13 -0400

    Add several tests to reach complete test coverage

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,53 +1,73 @@
 from unittest import TestCase
 import tinyapi
 from getpass import getpass
+from nose.tools import raises
 
 USERNAME = "tinyapi-test-account"
-session = tinyapi.Session(USERNAME, getpass())
+PASSWORD = getpass()
+
+class TestMainInterface(TestCase):
+    def test_login(self):
+        tinyapi.login(USERNAME, PASSWORD)
 
 class TestSession(TestCase):
+    def setUp(self):
+        self.session = tinyapi.Session(USERNAME, PASSWORD)
+
     def test_get_profile(self):
-        profile = session.get_profile()
+        profile = self.session.get_profile()
         assert(profile["username"] == USERNAME)
 
     def test_count_messages(self):
-        assert(session.count_messages() > 0)
+        assert(self.session.count_messages() > 0)
+
+    def test_count_sent_messages(self):
+        assert(self.session.count_sent_messages() > 0)
 
     def test_get_messages(self):
-        msgs = session.get_messages()
+        msgs = self.session.get_messages()
+        assert(len(msgs) > 0)
+
+    def test_get_sent_messages(self):
+        msgs = self.session.get_sent_messages()
         assert(len(msgs) > 0)
 
     def test_get_message(self):
-        msg = session.get_messages(count=1)[-1]
-        sub = session.get_message(msg["id"])
+        msg = self.session.get_messages(count=1)[-1]
+        sub = self.session.get_message(msg["id"])
         assert(type(sub["stats"]["total_opens"]) == int)
 
-        msg = session.get_messages(count=1, content=True)[-1]
-        sub = session.get_message(msg["id"])
+        msg = self.session.get_messages(count=1, content=True)[-1]
+        sub = self.session.get_message(msg["id"])
         assert(len(sub["content"]["html"]) > 0)
 
     def test_get_drafts(self):
-        drafts = session.get_drafts()
+        drafts = self.session.get_drafts()
         assert(type(drafts) == list)
 
     def test_count_urls(self):
-        assert(session.count_urls() > 0)
+        assert(self.session.count_urls() > 0)
 
     def test_get_urls(self):
-        assert(len(session.get_urls()) > 0)
+        assert(len(self.session.get_urls()) > 0)
 
     def test_get_message_urls(self):
-        message = session.get_messages()[-1]
-        urls = session.get_message_urls(message["id"])
+        message = self.session.get_messages()[-1]
+        urls = self.session.get_message_urls(message["id"])
         assert(len(urls) > 0)
 
     def test_count_subscribers(self):
-        assert(session.count_subscribers() > 0)
+        assert(self.session.count_subscribers() > 0)
 
     def test_get_subscribers(self):
-        assert(len(session.get_subscribers()) > 0)
+        assert(len(self.session.get_subscribers()) > 0)
 
     def test_get_subscriber(self):
-        subscriber = session.get_subscribers()[-1]
-        sub = session.get_subscriber(subscriber["id"])
+        subscriber = self.session.get_subscribers()[-1]
+        sub = self.session.get_subscriber(subscriber["id"])
         assert(type(sub["stats"]["last_sent_at"]) == int)
+
+class TestBadLogin(TestCase):
+    @raises(Exception)
+    def test_get_profile(self):
+        session = tinyapi.Session(USERNAME, "incorrect-password")
